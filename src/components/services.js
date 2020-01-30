588124bea06e8e326aae777b766caf58882470da
Dave Edwards <dave@dwedigital.com>
2020-01-30 16:20:12 +0000

    edited copy

--- src/components/services.js
+++ src/components/services.js
@@ -1,21 +1,16 @@
 import React from "react"
-import {Container, Row, Col} from 'react-bootstrap'
-
+import { Container, Row, Col } from "react-bootstrap"
 
 const Services = () => {
-
   return (
     <Container className="mt-5 px-0">
       <Row className="mt-5 align-items-center">
         <Col xs={{ span: 12 }} lg className=" justify-content-center">
-          <h1 className="mb-4 impact-header">
-          
-            ANALYTICS
-          </h1>
+          <h1 className="mb-4 impact-header">ANALYTICS</h1>
         </Col>
         <Col xs={{ span: 12 }} lg>
           {" "}
-          Analytics is the bedrock of scaling a business. Without proper
+          Analytics is the bedrock of growing a business. Without proper
           measurement you have no way of understanding what is working and what
           is not. We work with you to ensure you have access to the data you
           need to build a meaningful measurement framework. With this in place
@@ -27,40 +22,33 @@ const Services = () => {
         <Col xs={{ order: 12, span: 12 }} lg={{ order: 1 }}>
           {" "}
           A lot of small / medium businesses try to grow their customer base
-          without consindering how this affects day to day operations. We have
+          without consindering how this affects day-to-day operations. We have
           worked with several companies in the past and know the tried and
           tested journey & tools to put in place to reduce operational
           bottlenecks.
         </Col>
 
         <Col xs={{ order: 1, span: 12 }} lg={{ order: 12 }}>
-          <h1 className="mb-4 impact-header">
-   
-            OPERATIONS
-          </h1>
+          <h1 className="mb-4 impact-header">OPERATIONS</h1>
         </Col>
       </Row>
 
       <Row className="mt-5 align-items-center">
         <Col xs={{ span: 12 }} lg>
-          <h1 className="mb-4 impact-header">
-
-            GROWTH
-          </h1>
+          <h1 className="mb-4 impact-header">GROWTH</h1>
         </Col>
         <jsx></jsx>
         <Col xs={{ span: 12 }} lg>
           {" "}
           Growing an online business can be a minefield and prioritisation is
           key. We have extensive knowledge of online marketing to help you reach
-          qualified customers effectively. We look beyond just attracting new
-          leads and focus on decreasing cost per acquisiotn through Conversion
-          Rate Optimisation at every step of the customer journey.
+          qualified customers effectively. We look beyond clicks, impressions
+          and vanuty metrics. We focus on decreasing cost per acquistion &
+          return on investment by measuring and focusing on conversion.
         </Col>
       </Row>
     </Container>
   )
 }
-
 
 export default Services
--- src/pages/index.js
+++ src/pages/index.js
@@ -9,21 +9,14 @@ import Posts from '../components/latest_posts'
 const IndexPage = () => (
   <Layout>
     <SEO title="Home" />
-    <h1 className="mb-4 impact-header">Business Growth Consulting</h1>
+    <h1 className="mb-4 impact-header">We Grow Your Business</h1>
     <p>
-      DWE Digital is a digital consultancy focusing on scaling businesses. We
-      help clients define the Objectives & Key Results of their business and how
-      these will be measured. We then implement these in a measurement framework
-      that informs all aspects of the business.
+      DWE is a consultancy that thrives on growing businesses. We work hand-in-hand with clients to ensure the obvious questions have answers, can be measured and are actionable.
     </p>
     <p className="mt-3">
-      With proper analytics in place we then look across the operations of your
-      business to ensure you manage scale correctly.
-    </p>
+      DWE has worked across many client verticals ranging from e-commerce, retail, SaaS and product manafacturers. Chances are we have seen what your business does and what best practice looks like whilst working within the parameters of your company.
     <p className="mt-3">
-      When the foundations are set we then manage how to grow your customer base
-      through all your sales channels. Using analytics we focus on conversion
-      rate to minimise wasted marketing spend.
+      With a career spanning Digital Marketing, Analytics, Operations & Product Management our focus is helping you achieve business growth and managing how to scale your company across all digital touchpoints.
     </p>
     <div className="mt-5">
     <h1 className="section">Services</h1>
